1466838d427422c72fd49053cb32155b319a6185
hanwangyu <hanwangyu@foxmail.com>
2024-11-03 19:50:31 +0800

    feat: sign in form done

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,4 +1,5 @@
 import { DottedSeparator } from "@/components/dotted-separator";
+import { Button } from "@/components/ui/button";
 import {
     Card,
     CardContent,
@@ -8,12 +9,12 @@ import {
 import { Input } from "@/components/ui/input";
 
 export const SignInCard = () => {
-    return ( 
+    return (
         <Card className="w-full h-full md:w-[487px] border-none shadow-none">
             <CardHeader className="flex items-center justify-center text-center p-7">
                 <CardTitle className="text-2xl">
                     Welcome back!
-                </CardTitle>     
+                </CardTitle>
             </CardHeader>
 
             <div className="px-7">
@@ -23,15 +24,48 @@ export const SignInCard = () => {
             <CardContent className="p-7">
                 <form className="space-y-4">
                     <Input
-                      required
-                      type="email"
-                      value={""}
-                      onChange={() => {}}
-                      placeholder="Enter email address"
-                      disabled={false}
+                        required
+                        type="email"
+                        value={""}
+                        onChange={() => { }}
+                        placeholder="Enter email address"
+                        disabled={false}
                     />
+                    <Input
+                        required
+                        type="password"
+                        value={""}
+                        onChange={() => { }}
+                        placeholder="Enter password"
+                        disabled={false}
+                        min={8}
+                        max={256}
+                    />
+                    <Button disabled={false} size="lg" className="w-full">
+                        Login
+                    </Button>
                 </form>
             </CardContent>
-        </Card>    
+
+            <div className="px-7">
+                <DottedSeparator />
+            </div>
+            <CardContent className="p-7 flex flex-col gap-y-4">
+                <Button
+                  variant="secondary"
+                  size="lg"
+                  className="w=full"
+                >
+                    Login with Google
+                </Button>
+                <Button
+                  variant="secondary"
+                  size="lg"
+                  className="w=full"
+                >
+                    Login with Github
+                </Button>
+            </CardContent>
+        </Card>
     );
 }
