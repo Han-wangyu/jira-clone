20b0852c0266ef7c25fb178fb8b526cbfb6096a8
hanwangyu <hanwangyu@foxmail.com>
2024-11-13 12:18:05 +0800

    fix: remove request type in use-logout

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -1,16 +1,14 @@
 import { useMutation } from "@tanstack/react-query";
-import { InferRequestType, InferResponseType } from "hono";
+import { InferResponseType } from "hono";
 
 import { client } from "@/lib/rpc";
 
 type ResponseType = InferResponseType<typeof client.api.auth.logout["$post"]>;
-type RequestType = InferRequestType<typeof client.api.auth.logout["$post"]>;
 
 export const useLogout = () => {
     const mutation = useMutation<
         ResponseType,
-        Error,
-        RequestType
+        Error
     >({
         mutationFn: async () => {
             const response = await client.api.auth.logout["$post"]();
