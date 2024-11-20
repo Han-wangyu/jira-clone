b03297ee301747da1a21c8f1757f3fc40b296f9c
hanwangyu <hanwangyu@foxmail.com>
2024-11-20 16:50:02 +0800

    feat: protect auth router with server action instead of api & client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,11 @@
-"use client"
-
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
-import { Button } from "@/components/ui/button";
-
-import { useCurrent } from "@/features/auth/api/use-current";
-import { useLogout } from "@/features/auth/api/use-logout";
+import { getCurrent } from "@/features/auth/actions";
 import { UserButton } from "@/features/auth/components/user-button";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  const router = useRouter();
-  const { data, isLoading } = useCurrent();
-  const { mutate } = useLogout();
+export default async function Home() {
+  const user = await getCurrent();
 
-  useEffect(() => {
-    if (!data && !isLoading) {
-      router.push("/sign-in");
-    }
-  }, [data, isLoading, router]);
+  if (!user) redirect("/sign-in");
 
   return (
     <div>
