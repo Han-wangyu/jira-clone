5cbc75cc6d379c7a6f76592a564b8fe4893b1014
hanwangyu <hanwangyu@foxmail.com>
2024-11-21 10:05:18 +0800

    feat: create dashboard page&layout in app folder

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,22 @@
+interface DashboardLayoutProps {
+    children: React.ReactNode;
+};
+
+const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+    return (
+        <div className="min-h-screen">
+            <div className="flex w-full h-full">
+                <div className="lg:pl-[264px]">
+                    <div className="mx-auto max-w-screen-2xl h-full">
+                        {/* TODO: Navbar */}
+                        <main>
+                            { children }
+                        </main>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default DashboardLayout;
